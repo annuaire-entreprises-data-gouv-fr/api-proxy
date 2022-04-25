81bc675aab10e444cc941cb33dea4e65dd1434ed
xavier jouppe <xajouppe@gmail.com>
2022-04-25 14:58:17 +0200

    update host name

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
+    '^.+\\.(t|j)s?$': ['@swc/jest'],
   },
 };
