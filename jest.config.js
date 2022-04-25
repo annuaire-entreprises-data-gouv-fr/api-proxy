3aaca3c4dbc944c9108d037c3397b7f2be25fcb5
xavier jouppe <xajouppe@gmail.com>
2022-04-25 11:19:42 +0200

    fix: unit test

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+};
