b73101849a2b8425d83dbec6c4177cbb34561692
xavier jouppe <xajouppe@gmail.com>
2022-05-05 16:37:54 +0200

    fix: remove unused variable

--- src/utils/auth/api/index.ts
+++ src/utils/auth/api/index.ts
@@ -1,12 +1,8 @@
 import { AxiosRequestConfig } from 'axios';
 import inpiAPIAuth from './provider';
 
-const authApiClient = async (
-  route: string,
-  options?: AxiosRequestConfig,
-  withCache = true
-) => {
-  return await inpiAPIAuth.authenticatedClient(route, options, withCache);
+const authApiClient = async (route: string, options?: AxiosRequestConfig) => {
+  return await inpiAPIAuth.authenticatedClient(route, options);
 };
 
 export { authApiClient };
