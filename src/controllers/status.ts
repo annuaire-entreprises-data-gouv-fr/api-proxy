93757dad17fe72c46c43e9d21b21598b564ef881
xavier jouppe <xajouppe@gmail.com>
2025-02-27 15:55:39 +0100

    chore: use actes token for monitoring rne

--- src/controllers/status.ts
+++ src/controllers/status.ts
@@ -1,12 +1,13 @@
 import { verifySiren } from '../models/siren-and-siret';
 import { Request, Response } from 'express';
-import { fetchImmatriculationFromAPIRNE } from '../clients/inpi/api-rne';
+import { listDocumentsRne } from '../clients/inpi/api-rne-documents';
 
 export const apiRneStatusController = async (req: Request, res: Response) => {
   try {
     const dummySiren = verifySiren('552032534');
-    const useCache = false;
-    await fetchImmatriculationFromAPIRNE(dummySiren, useCache);
+
+    // uses actes for monitoring as they are rarely rate limited
+    await listDocumentsRne(dummySiren);
     res.send({ message: 'ok', status: 200 });
   } catch (e: any) {
     const status = e.status || 500;
