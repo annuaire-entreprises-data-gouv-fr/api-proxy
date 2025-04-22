dfa2aceabcfca4c34c342ed874b715e86794f6be
Robin Monnier <monnier.robin@gmail.com>
2025-04-22 16:47:33 +0200

    feat: upgrade express to version 5.1.0 and fix sonde (#139)
    * feat: upgrade express to version 5.1.0 and fix sonde
    
    * fix: use open data rne rather than document for sonde

--- src/controllers/status.ts
+++ src/controllers/status.ts
@@ -1,13 +1,12 @@
-import { verifySiren } from '../models/siren-and-siret';
 import { Request, Response } from 'express';
-import { listDocumentsRne } from '../clients/inpi/api-rne-documents';
+import { fetchRneAPI } from '../models/rne';
+import { verifySiren } from '../models/siren-and-siret';
 
 export const apiRneStatusController = async (req: Request, res: Response) => {
   try {
     const dummySiren = verifySiren('552032534');
 
-    // uses actes for monitoring as they are rarely rate limited
-    await listDocumentsRne(dummySiren);
+    await fetchRneAPI(dummySiren);
     res.send({ message: 'ok', status: 200 });
   } catch (e: any) {
     const status = e.status || 500;
