1e0ffdd2819d0e6c50a2169c82bc84d26e4ca845
Alan Chauchet <alan.chauchet@gmail.com>
2025-10-13 18:00:28 +0200

    fix(tva): uses vatNumber instead of tva field

--- src/clients/tva/index.ts
+++ src/clients/tva/index.ts
@@ -1,28 +1,32 @@
-import constants from '../../constants';
-import { TVANumber } from '../../models/siren-and-siret';
-import { httpGet } from '../../utils/network';
-import { getOrSetWithCacheExpiry } from '../../utils/network/storage/smart-cache-storage';
-import routes from '../urls';
+import constants from "../../constants";
+import type { TVANumber } from "../../models/siren-and-siret";
+import { httpGet } from "../../utils/network";
+import { getOrSetWithCacheExpiry } from "../../utils/network/storage/smart-cache-storage";
+import routes from "../urls";
 
 const ONE_MONTH_MS = 30 * 24 * 60 * 60 * 1000;
 const ONE_WEEK_MS = 7 * 24 * 60 * 60 * 1000;
 
 export const clientTVA = (tvaNumber: TVANumber): Promise<string> => {
-  const encodedTvaNumber = encodeURIComponent(tvaNumber);
-  const url = `${routes.tva}${encodedTvaNumber}`;
+	const encodedTvaNumber = encodeURIComponent(tvaNumber);
+	const url = `${routes.tva}${encodedTvaNumber}`;
 
-  const callback = () => httpGet<{ userError: string, tva?: string }>(url, { timeout: constants.timeout.XXL, useCache: false }).then((res) => {
-      if (!["VALID", "INVALID"].includes(res.userError)) {
-        throw new Error(res.userError);
-      }
+	const callback = () =>
+		httpGet<{ userError: string; isValid: boolean; vatNumber?: string }>(url, {
+			timeout: constants.timeout.XXL,
+			useCache: false,
+		}).then((res) => {
+			if (!["VALID", "INVALID"].includes(res.userError)) {
+				throw new Error(res.userError);
+			}
 
-      return { tva: res.tva || null };
-    });
+			return { tva: res.isValid ? (res.vatNumber ?? null) : null };
+		});
 
-  return getOrSetWithCacheExpiry(
-    `tva:${tvaNumber}`,
-    callback,
-    ONE_MONTH_MS,
-    ONE_WEEK_MS
-  );
+	return getOrSetWithCacheExpiry(
+		`tva:${tvaNumber}`,
+		callback,
+		ONE_MONTH_MS,
+		ONE_WEEK_MS,
+	);
 };
