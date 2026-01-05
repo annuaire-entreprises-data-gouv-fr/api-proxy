50f6ef1cd3f8ed69f32642a87c0dedc92b350863
Alan Chauchet <alan.chauchet@gmail.com>
2026-01-05 15:16:34 +0100

    refactor(grist): remove logInGrist function and add sentry errors

--- src/clients/external-tooling/grist.ts
+++ src/clients/external-tooling/grist.ts
@@ -1,7 +1,5 @@
 import constants from "../../constants";
-import { Exception } from "../../models/exceptions";
 import httpClient from "../../utils/network";
-import { logErrorInSentry } from "../../utils/sentry";
 import routes from "../urls";
 
 type IGristRecords = {
@@ -29,29 +27,6 @@ function getGristUrl(tableKey: keyof typeof gristTables) {
   return `${routes.tooling.grist}${gristIds.docId}/tables/${gristIds.tableId}/records`;
 }
 
-export async function logInGrist(
-  tableKey: keyof typeof gristTables,
-  data: unknown[]
-) {
-  try {
-    await httpClient({
-      method: "POST",
-      url: getGristUrl(tableKey),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.GRIST_API_KEY}`,
-      },
-      data: {
-        records: data.map((d) => ({ fields: d })),
-      },
-      timeout: constants.timeout.XXL,
-    });
-  } catch (error) {
-    logErrorInSentry(new LogInGristException({ cause: error }));
-    throw error;
-  }
-}
-
 export async function readFromGrist(
   tableKey: keyof typeof gristTables,
   timeout: number = constants.timeout.XXL
@@ -66,13 +41,4 @@ export async function readFromGrist(
   });
 
   return records.map((r: any) => r.fields);
-}
-
-class LogInGristException extends Exception {
-  constructor(args: { cause?: any }) {
-    super({
-      ...args,
-      name: "LogInGristException",
-    });
-  }
 }
--- src/controllers/feature-flags.ts
+++ src/controllers/feature-flags.ts
@@ -2,9 +2,9 @@ import type { NextFunction, Request, Response } from "express";
 import {
   cacheFeatureFlags,
   clientFeatureFlags,
-  type FeatureFlagValue,
   readFeatureFlagsFromCache,
 } from "../clients/feature-flags";
+import { logErrorInSentry } from "../utils/sentry";
 
 export const featureFlagsController = async (
   _: Request,
@@ -21,36 +21,39 @@ export const featureFlagsController = async (
 
 const FIVE_MINUTES_MS = 5 * 60 * 1000;
 
-const fetchAndCacheFeatureFlags = async (): Promise<{
-  [key: string]: FeatureFlagValue;
-}> => {
+const fetchAndCacheFeatureFlags = async () => {
+  // biome-ignore lint/suspicious/noConsole: needed for logging
+  console.log("💽[server]: Polling feature flags...");
+
   const featureFlags = await clientFeatureFlags().catch((e) => {
     // biome-ignore lint/suspicious/noConsole: needed for logging
     console.error("💽[server]: Error polling feature flags:", e);
+
+    logErrorInSentry(
+      `Error polling feature flags : ${e instanceof Error ? e.message : "Unknown error"}`
+    );
     return {};
   });
 
   await cacheFeatureFlags(featureFlags).catch((e) => {
     // biome-ignore lint/suspicious/noConsole: needed for logging
     console.error("💽[server]: Error caching feature flags:", e);
+
+    logErrorInSentry(
+      `Error caching feature flags : ${e instanceof Error ? e.message : "Unknown error"}`
+    );
   });
+
+  // biome-ignore lint/suspicious/noConsole: needed for logging
+  console.log("💽[server]: Feature flags cached:", featureFlags);
 
   return featureFlags;
 };
 
 export const startPollingFeatureFlags = (): NodeJS.Timeout => {
-  // biome-ignore lint/suspicious/noConsole: needed for logging
-  console.log("💽[server]: Polling feature flags...");
-
   fetchAndCacheFeatureFlags();
 
-  return setInterval(async () => {
-    // biome-ignore lint/suspicious/noConsole: needed for logging
-    console.log("💽[server]: Polling feature flags...");
-
-    const featureFlags = await fetchAndCacheFeatureFlags();
-
-    // biome-ignore lint/suspicious/noConsole: needed for logging
-    console.log("💽[server]: Feature flags cached:", featureFlags);
+  return setInterval(() => {
+    fetchAndCacheFeatureFlags();
   }, FIVE_MINUTES_MS);
 };
