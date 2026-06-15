76796c5f2021e82ae8c99c90e7a559ec4107c8d4
AlanChauchet <alan.chauchet@gmail.com>
2026-06-15 14:17:33 +0200

    feat(tva): removes tva endpoint (#258)

--- src/models/siren-and-siret/index.ts
+++ src/models/siren-and-siret/index.ts
@@ -10,21 +10,6 @@ import {
  */
 type Brand<K, T> = K & { __brand: T };
 
-export type TVANumber = Brand<string, "TVANumber">;
-
-export const isTVANumber = (slug: string): slug is TVANumber =>
-  !!slug.match(/^\d{11}$/g);
-
-/**
- * throw an exception if a string is not a TVA Number
- */
-export const verifyTVANumber = (slug: string): TVANumber => {
-  if (isTVANumber(slug)) {
-    return slug;
-  }
-  throw new Error("Not a valid TVANumber");
-};
-
 export type Siren = Brand<string, "Siren">;
 export type Siret = Brand<string, "Siren">;
 
